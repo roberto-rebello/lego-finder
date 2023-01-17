c0fd4dd7c9c9c8d1d1214f02fbf49112f23cc867
Roberto Rebello <roberto-rebello@github.com>
2023-01-17 08:09:12 -0300

    Fixing product not found issue

--- lego.py
+++ lego.py
@@ -14,7 +14,12 @@ LEGO_CODE = sys.argv[1]
 POSTAL_CODE = sys.argv[2]
 
 lego_code_response = requests.get(BASE_URL+str(LEGO_CODE))
-product_id = re.search('productid="([0-9]+)"', lego_code_response.text)[1]
+search_results = re.findall('productid="([0-9]+)"', lego_code_response.text)
+if len(search_results) > 1:
+    print("Produto não encontrado")
+    exit()
+else:
+    product_id = int(search_results[0])
 
 lego_product_search = requests.get(BASE_URL+PRODUCT_SEARCH_URI, params={"fq":"productId:{}".format(product_id)})
 item_id = lego_product_search.json()[0]["items"][0]["itemId"]
@@ -36,6 +41,7 @@ if len(slas) > 0:
 
         print()
 else:
+    print("Produto indisponivel no CEP informado. Procurando em outras lojas LEGO...")
     with open("stores.json", "r") as lego_stores_file:
         lego_stores = json.load(lego_stores_file)
         stores = []
